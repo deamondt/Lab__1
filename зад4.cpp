cae8f2de3f654c1c83af7d988c03e878370c4039
deamondt <deamondt@users.noreply.github.com>
2016-06-05 17:09:33 +0300

    Add files via upload

---
+++ зад4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+	int main ( )
+{
+	float s , y ;
+	
+	printf ( "\ nVvedite 'g': " ) ;
+	scanf ( "%f" , &s ) ;
+	
+	if  ( s <= 1 )  {
+		y = 1 ;
+		printf ( "\ nf(s)= %.3f" , y ) ;
+	} else {
+		if  ( ( 0 < s ) && ( s <= 2 ) )  {
+			y = s ;
+			printf ( "\ nf(s)= %.3f" , y ) ;
+		} else {
+			y = s * s * s ;
+			printf ( "\ nf(s)= %.3f" , y ) ;
+		}
+	}
+	
+	return 0 ;
+}
