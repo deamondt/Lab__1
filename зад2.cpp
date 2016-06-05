a0fa80cc88947cada632527ac18936d983c6cd5a
deamondt <deamondt@users.noreply.github.com>
2016-06-05 23:47:15 +0300

    Add files via upload

---
+++ зад2.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void riv ( int x ) ;
+
+int main ( )
+{
+	int x1 , x2 , x3 ;
+	
+	printf ("\nvvedite 3 chesla sherez probel : " ) ;
+	scanf ( "%d %d %d" , &x1 , &x2 , &x3 ) ;	
+	printf ( "\n\nshisla v diapazone ot -7 do 14:" ) ;
+	riv ( x1 ) ;
+	riv ( x2 ) ;
+	riv ( x3 ) ;
+	
+	return 0 ;
+}
+
+void riv  ( int x ) 
+{
+	int p ;
+	if  ( ( x >= -7 ) && ( x <= 14 ) ) {
+		printf  ( "  %d" , x ) ;
+	} else {}
+}
